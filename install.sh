32c7baac6e81e61643b076d4bd54f79471c4417b
Jerry Teng <zuyuangteng@gmail.com>
2021-02-18 19:58:36 -0600

    updated dotfiles

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -sf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+ln -sf $HOME/dotfiles/.aliases $HOME/.aliases
+ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -sf $HOME/dotfiles/.gdbinit $HOME/.gdbinit
