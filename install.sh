f03a0c3967601628e2c2e1e1196a494e1c351a76
Jerry Teng <zuyuangteng@gmail.com>
2021-09-20 23:39:43 -0500

    .

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# additional tools for binaries
-pip3 install capstone unicorn keystone-engine ropper
-
 # gef
-if [ ! -f !/.gdbinit-gef.py ]; then
-    wget -O ~/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
-fi
+#if [ ! -f !/.gdbinit-gef.py ]; then
+#    wget -O ~/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
+#fi
 
 if [ ! -d ~/.oh-my-zsh ]; then
     curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+fi
+
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
 
 if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
@@ -23,12 +24,10 @@ fi
 files=(
     ".aliases" 
     ".functions" 
-    ".gdbinit" 
     ".gitconfig" 
-    ".git_aliases" 
-    ".zshenv" 
+    ".p10k.zsh"
     ".vimrc" 
-    ".xinitrc"
+    ".zshenv" 
     ".zshrc"
 )
 
@@ -38,24 +37,23 @@ done
 
 [ ! -d $HOME/.vim/undodir ] && mkdir $HOME/.vim/undodir
 
-[ ! -d $HOME/.config ] && mkdir $HOME/.config
-[ ! -d $HOME/.config/VSCodium/User ] && mkdir -p $HOME/.config/VSCodium/User
+#[ ! -d $HOME/.config ] && mkdir $HOME/.config
+#[ ! -d $HOME/.config/VSCodium/User ] && mkdir -p $HOME/.config/VSCodium/User
 
-ln -sf $(pwd)/config/VSCodium/product.json $HOME/.config/VSCodium/product.json
-ln -sf $(pwd)/config/VSCodium/User/settings.json $HOME/.config/VSCodium/User/settings.json
+#ln -sf $(pwd)/config/VSCodium/product.json $HOME/.config/VSCodium/product.json
+#ln -sf $(pwd)/config/VSCodium/User/settings.json $HOME/.config/VSCodium/User/settings.json
 
-[ -d $HOME/.config/alacritty ] && rm -fr $HOME/.config/alacritty
-ln -sf $(pwd)/config/alacritty $HOME/.config/alacritty
+#[ -d $HOME/.config/alacritty ] && rm -fr $HOME/.config/alacritty
+#ln -sf $(pwd)/config/alacritty $HOME/.config/alacritty
 
-[ -d $HOME/.config/nitrogen ] && rm -fr $HOME/.config/nitrogen
-ln -sf $(pwd)/config/nitrogen $HOME/.config/nitrogen
+#[ -d $HOME/.config/nitrogen ] && rm -fr $HOME/.config/nitrogen
+#ln -sf $(pwd)/config/nitrogen $HOME/.config/nitrogen
 
-[ -d $HOME/.config/picom ] && rm -fr $HOME/.config/picom
-ln -sf $(pwd)/config/picom $HOME/.config/picom
+#[ -d $HOME/.config/picom ] && rm -fr $HOME/.config/picom
+#ln -sf $(pwd)/config/picom $HOME/.config/picom
 
-[ -d $HOME/.config/qtile ] && rm -fr $HOME/.config/qtile
-ln -sf $(pwd)/config/qtile $HOME/.config/qtile
+#[ -d $HOME/.config/qtile ] && rm -fr $HOME/.config/qtile
+#ln -sf $(pwd)/config/qtile $HOME/.config/qtile
 
-[ -d $HOME/.config/qtile ] && rm -fr $HOME/.config/qtile
-ln -sf $(pwd)/config/qtile $HOME/.config/qtile
-
+#[ -d $HOME/.config/qtile ] && rm -fr $HOME/.config/qtile
+#ln -sf $(pwd)/config/qtile $HOME/.config/qtile
--- packages.sh
+++ packages.sh
@@ -15,8 +15,6 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 packages=(
-    code
-    gdb
     git 
     htop
     neofetch
