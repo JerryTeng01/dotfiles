cc16d1ce25d569f1db3427603e66f86bb1a89ecc
Jerry Teng <zuyuangteng@gmail.com>
2021-10-16 16:50:53 -0500

    .

--- completion.zsh
+++
@@ -1,9 +0,0 @@
-zmodload zsh/complist
-
-bindkey -M menuselect 'h' vi-backward-char
-bindkey -M menuselect 'k' vi-up-line-or-history
-bindkey -M menuselect 'j' vi-down-line-or-history
-bindkey -M menuselect 'l' vi-forward-char
-
-autoload -U compinit; compinit
-_comp_options+=(globdots) # With hidden files
